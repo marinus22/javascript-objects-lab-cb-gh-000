d4e9d8eb7473d4ff9c728a7387e73e5320143979
Sara McCarthy <sc.mccarthy1123@gmail.com>
2018-07-03 13:05:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var recipes = new Object({sugar: '1 cup'});
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  
+  return Object.assign({}, object, {[key]: value});
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-
+  recipes.flour = '2 cups';
+  return recipes;
 }
 
 function deleteFromObjectByKey(object, key) {
